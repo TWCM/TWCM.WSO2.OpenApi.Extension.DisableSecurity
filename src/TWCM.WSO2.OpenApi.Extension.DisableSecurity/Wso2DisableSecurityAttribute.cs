25fae89a17b107c83efca108aa2a522a865cdfbb
tommywu <tommywu@TOMMYWU8F77>
2024-10-03 17:43:33 +0800

    Add project files.

---
+++ src/TWCM.WSO2.OpenApi.Extension.DisableSecurity/Wso2DisableSecurityAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TWCM.WSO2.OpenApi.Extension.DisableSecurity
+{
+  
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class Wso2DisableSecurityAttribute : Attribute
+    {
+        public bool IsEnable { get; set; } = false;
+        public Wso2DisableSecurityAttribute(bool isEnable=false)
+        {
+            this.IsEnable = isEnable;
+
+        }    
+    }
+}
---
+++ src/TWCM.WSO2.OpenApi.Extension.DisableSecurity/Wso2DisableSecurityOperationFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace TWCM.WSO2.OpenApi.Extension.DisableSecurity
+{
+    public class Wso2DisableSecurityOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+
+            var classAttributes = context.MethodInfo.DeclaringType!.GetCustomAttributes(true).OfType<Wso2DisableSecurityAttribute>();
+
+            var methodAttributes = context.MethodInfo.GetCustomAttributes(true).OfType<Wso2DisableSecurityAttribute>();
+
+            //class attribute will be ignored if method attribute exist
+            if (methodAttributes.Any()) {
+
+                operation.Extensions.Add("x-wso2-disable-security", new OpenApiBoolean(methodAttributes.First().IsEnable));
+
+
+                return;
+            }
+            if (classAttributes.Any())
+            {
+
+
+                operation.Extensions.Add("x-wso2-disable-security", new OpenApiBoolean(classAttributes.First().IsEnable));
+
+
+                return;
+            }
+        }
+    }
+}
